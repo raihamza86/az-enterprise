a65b99b2ce5600489c72a0c95125f389857298ba
malikumair687 <malikumairbwp786@gmail.com>
2025-04-12 12:38:44 +0500

    services and SErvice Detail Page

---
+++ frontend/src/pages/DetailServic.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import DetailHero from '../components/detailServices/DetailHero';
+import CategoryDetail from '../components/detailServices/CategoryDetail';
+
+
+const DetailServic = () => {
+  return (
+    <div>
+        <DetailHero/>
+        <CategoryDetail/>
+    </div>
+  )
+}
+
+export default DetailServic;
